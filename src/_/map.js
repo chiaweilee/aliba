0292030fcd329d7d2bfd446e9e3662ad292050f6
+v <ljw@live.jp>
2019-03-16 21:26:31 +0800

    Delete map.js

--- src/_/map.js
+++
@@ -1,9 +0,0 @@
-export default (collection, iteratee) => {
-    if (Array.isArray(collection)) {
-        return collection.map(iteratee);
-    }
-    return Object.keys(collection).map(key => {
-        return iteratee(collection[key], key);
-    });
-};
-//# sourceMappingURL=map.js.map
