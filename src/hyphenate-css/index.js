6007c73d8645327c98a4943f90255f4b0233edc2
+v <ljw@live.jp>
2019-03-16 21:27:10 +0800

    Delete index.js

--- src/hyphenate-css/index.js
+++
@@ -1,4 +0,0 @@
-import map from '../_/map';
-import hyphenateStyleName from '../shared/hyphenateStyleName';
-export default (style) => map(style, (value, name) => `${hyphenateStyleName(name)}:${value};`).join('');
-//# sourceMappingURL=index.js.map
