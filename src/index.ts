2708142c9dd1dfb7772b0f422526e3da03c6145c
jv <ljw@live.jp>
2019-03-16 21:20:34 +0800

    build: 0.2.1

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
+export { default as forEach } from './_/forEach';
+export { default as map } from './_/map';
 export { default as hyphenateStyleName } from './shared/hyphenateStyleName';
 export { default as hyphenateCss } from './hyphenate-css';
