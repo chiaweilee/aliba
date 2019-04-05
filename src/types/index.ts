faee775bf097db6e799852c2f397648d7831b4d7
jv <ljw@live.jp>
2019-04-05 09:09:52 +0800

    ts: update

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,5 @@
 export type Collection = object | Array<any>;
-export type Iteratee = (value: any, key: string | number) => any;
+export type Iteratee = (value: any, key?: string | number) => any;
 export type StateChange = object;
 export type Updater = (state: object, props?: object) => StateChange;
 
