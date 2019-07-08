a5e856710e147b37368af3be0dcd25d394fe07e2
jv <ljw@live.jp>
2019-07-08 12:47:08 +0800

    refactor: lodash

--- src/_/forEach.ts
+++ src/_/forEach.ts
@@ -1,16 +1,1 @@
-import { Collection, Iteratee } from '../../types';
-import isNotArrayObject from './isNotArrayObject';
-
-export default (collection: Collection, iteratee: Iteratee): void => {
-  if (Array.isArray(collection)) {
-    return collection.forEach(iteratee);
-  }
-
-  if (!isNotArrayObject(collection)) {
-    return;
-  }
-
-  Object.keys(collection).forEach(key => {
-    iteratee(collection[key], key);
-  });
-};
+export default require('lodash/forEach');
--- src/_/map.ts
+++ src/_/map.ts
@@ -1,16 +1,1 @@
-import { Collection, Iteratee } from '../../types';
-import isNotArrayObject from './isNotArrayObject';
-
-export default (collection: Collection, iteratee: Iteratee): Array<any> => {
-  if (Array.isArray(collection)) {
-    return collection.map(iteratee);
-  }
-
-  if (!isNotArrayObject(collection)) {
-    return [];
-  }
-
-  return Object.keys(collection).map(key => {
-    return iteratee(collection[key], key);
-  });
-};
+export default require('lodash/map');
